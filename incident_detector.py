f06ef9ec65955d59bdcb1b6316b88728bba343c6
succe3d <nehipaul@gmail.com>
2024-07-24 13:46:59 -0500

    Add Incident detection Script

--- incident_detector.py
+++ incident_detector.py
@@ -8,6 +8,24 @@ client = boto3.client('logs')
 # CloudWatch Log group name
 log_group_name = '/aws/lambda/incident_logs'
 
+# Specify your AWS region
+region = 'us-east-1'
+
+client = boto3.client('logs', region_name=region)
+
+def log_incident():
+    response = client.put_log_events(
+        logGroupName='your-log-group',
+        logStreamName='your-log-stream',
+        logEvents=[
+            {
+                'timestamp': int(time.time() * 1000),
+                'message': 'Incident detected!'
+            },
+        ],
+    )
+    print(response)
+
 # Function to simulate incident detection
 def log_incident():
     timestamp = int(time.time() * 1000)
