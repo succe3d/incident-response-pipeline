3690b22e4db5fa0b46964f01f9ff30280520dab2
succe3d <nehipaul@gmail.com>
2024-07-25 00:23:37 -0500

    Add Lambda Function

--- terraform/incident_detector.py
+++ terraform/incident_detector.py
@@ -2,20 +2,18 @@ import boto3
 import json
 import datetime
 
-
 def create_log_group_and_stream(client, log_group_name, log_stream_name):
     try:
         client.create_log_group(logGroupName=log_group_name)
         print(f'Log group {log_group_name} created.')
     except client.exceptions.ResourceAlreadyExistsException:
         print(f'Log group {log_group_name} already exists.')
-
+    
     try:
         client.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
         print(f'Log stream {log_stream_name} created.')
     except client.exceptions.ResourceAlreadyExistsException:
         print(f'Log stream {log_stream_name} already exists.')
-
 
 def log_incident():
     client = boto3.client('logs', region_name='us-west-2')  # Specify the region here
@@ -43,7 +41,6 @@ def log_incident():
     )
 
     print(response)
-
 
 if __name__ == "__main__":
     log_incident()
