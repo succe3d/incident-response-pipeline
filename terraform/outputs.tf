393d3db8aea4f9bbb8e81ee0df647d44414a91f3
succe3d <nehipaul@gmail.com>
2024-07-24 20:59:59 -0500

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "log_group_name" {
+  value = aws_cloudwatch_log_group.incident_log_group.name
+}
+
+output "log_stream_name" {
+  value = aws_cloudwatch_log_stream.incident_log_stream.name
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.incident_detector.function_name
+}
